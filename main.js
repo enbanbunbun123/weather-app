e1b28ee4c80a39bb91dc77a74faa4c43828d65ae
enbanbunbun123 <enbanbunbun-123@docomo.ne.jp>
2023-10-22 22:43:29 +0900

    feat: 東京の天気が取得できるように実装

--- main.js
+++ main.js
@@ -1,12 +1,38 @@
 const request = require("request");
 const dotenv = require("dotenv").config();
 
+const cities = {
+  tokyo: { lat: 35.6895, lon: 139.6917 },
+};
+
+const cityName = process.argv[2].toLowerCase();
+const cityData = cities[cityName];
+
+if (!cityData) {
+  console.log("City not found");
+  process.exit(1);
+}
+
 const options = {
-  url: `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&units=metric&appid=${process.env.API_KEY}`,
+  url: `https://api.openweathermap.org/data/2.5/weather?lat=${cityData.lat}&lon=${cityData.lon}&units=metric&appid=${process.env.API_KEY}`,
   method: "GET",
   json: true,
 };
 
 request(options, (error, res, body) => {
-  console.log(body.main.temp);
+  if (error) {
+    console.error("Error fetching weather data:", error);
+    process.exit(1);
+  }
+
+  if (res.statusCode !== 200) {
+    console.error("API returned an error:", body.message);
+    process.exit(1);
+  }
+
+  console.log(
+    `The temperature in ${
+      cityName.charAt(0).toUpperCase() + cityName.slice(1)
+    } is ${body.main.temp}°C.`
+  );
 });
